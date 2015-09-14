cfddc9474df9195d61d8fafea6430c0f02d9660d
AlexanderC <alexander.moldova@gmail.com>
2015-09-14 16:17:21 +0300

    tmp remove travis

--- test/bin/generate_missing_tests.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-source $(dirname $0)/_head.sh
-
-### Install NPM deps ###
-
-__CMD='babel-node '${__SCRIPT_PATH}'/helper/generate_missing_tests.js `pwd -P`'
-
-subpath_run_cmd ${__SRC_PATH} "$__CMD"
