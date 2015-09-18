ab401b0ad8fdce79670bda7b6828df0c73aea370
AlexanderC <alexander.moldova@gmail.com>
2015-09-18 16:25:55 +0300

    remove root parameters

--- src/DeepHelloWorld/Frontend/bootstrap.js
+++ src/DeepHelloWorld/Frontend/bootstrap.js
@@ -1,3 +1,7 @@
+/**
+ * Created by AlexanderC on 9/18/15.
+ */
+
 'use strict';
 
 export default function deepHelloWorld() {
@@ -8,9 +12,7 @@ export function configLoad() {
   let promise = new Promise(() => {});
   let deepSecurity = DeepFramework.Kernel.get('security');
 
-  deepSecurity.anonymousLogin(function(token) {
-    promise.resolve();
-  });
+  deepSecurity.anonymousLogin((token) => promise.resolve(token));
 
   return promise;
 }
