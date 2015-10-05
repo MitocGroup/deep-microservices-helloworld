6f7b7333251a809f71fc5a1c1e057276dce2288c
vcernomschi <vcernomschi@vcernomschis-MacBook-Pro.local>
2015-10-05 20:25:06 +0300

    Add logging to coverage.sh

--- test/bin/coverage.sh
+++ test/bin/coverage.sh
@@ -7,16 +7,20 @@ __CMD='npm run coverage'
 subpath_run_cmd ${__SRC_PATH} "$__CMD"
 
 ### Merge Coverage results ###
+echo "Starting combining"
+
 istanbul-combine -d ${__COVERAGE_PATH} -r lcovonly -p both \
   ${__SRC_PATH}/*/Tests/Frontend/coverage/*/coverage-final.json
   ${__SRC_PATH}/*/Tests/Backend/coverage/*/coverage-final.json
 
-### Upload Coverage info to Codacy ###
-#cat ${__COVERAGE_PATH}"/lcov.info" | codacy-coverage
-#cat ${__COVERAGE_PATH}"/lcov.info" | coveralls
+echo "Done combining"
+## Upload Coverage info to Codacy ###
+cat ${__COVERAGE_PATH}"/lcov.info" | codacy-coverage
+cat ${__COVERAGE_PATH}"/lcov.info" | coveralls
 
+echo "Starting cleaup"
 ### Cleanup! ###
-#__CMD='rm -rf ./coverage'
-#
-#subpath_run_cmd ${__SRC_PATH} "$__CMD"
-#subpath_run_cmd ${__COVERAGE_PATH} "$__CMD"
+__CMD='rm -rf ./coverage'
+
+subpath_run_cmd ${__SRC_PATH} "$__CMD"
+subpath_run_cmd ${__COVERAGE_PATH} "$__CMD"
