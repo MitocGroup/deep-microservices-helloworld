d11b6d687bd0103652aa276adeafe488363b965f
vcernomschi <vcernomschi@vcernomschis-MacBook-Pro.local>
2015-10-06 23:03:11 +0300

    Fix shells

--- test/bin/coverage.sh
+++ test/bin/coverage.sh
@@ -7,25 +7,13 @@ __CMD='npm run coverage'
 subpath_run_cmd ${__SRC_PATH} "$__CMD"
 
 ### Merge Coverage results ###
-istanbul-combine -d ${__COVERAGE_PATH} -r lcovonly -p both \
+istanbul-combine -d ${__COVERAGE_PATH} -r lcov -p both \
   ${__SRC_PATH}*/Tests/Frontend/coverage/*/*.json \
   ${__SRC_PATH}*/Tests/Backend/coverage/*.json
 
-OLD_PATH="Tests\/Backend\/Frontend"
-NEW_PATH="Frontend"
-sed -i "s/$OLD_PATH/$NEW_PATH/g" ${__COVERAGE_PATH}"/lcov.info"
-
-## Upload Coverage info to Codacy ###
+### Upload Coverage info to Codacy ###
 cat ${__COVERAGE_PATH}"/lcov.info" | codacy-coverage
 cat ${__COVERAGE_PATH}"/lcov.info" | coveralls
-
-#OLD_PATH="Tests\/Backend\/Frontend"
-#NEW_PATH="Frontend"
-#sed "s/$OLD_PATH/$NEW_PATH/g" ${__COVERAGE_PATH}"/lcov.info" > ${__COVERAGE_PATH}"/prepared_report.info"
-#
-#### Upload Coverage info to Codacy ###
-#cat ${__COVERAGE_PATH}"/prepared_report.info" | codacy-coverage
-#cat ${__COVERAGE_PATH}"/prepared_report.info" | coveralls
 
 ### Cleanup! ###
 #remove all generated reports
