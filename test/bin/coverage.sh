65c42170bffac26659a11987b89be8f9ee257ec9
vcernomschi <vcernomschi@vcernomschis-MacBook-Pro.local>
2015-10-06 14:54:03 +0300

    Update sed command

--- test/bin/coverage.sh
+++ test/bin/coverage.sh
@@ -13,12 +13,19 @@ istanbul-combine -d ${__COVERAGE_PATH} -r lcovonly -p both \
 
 OLD_PATH="Tests\/Backend\/Frontend"
 NEW_PATH="Frontend"
-sed "s/$OLD_PATH/$NEW_PATH/g" ${__COVERAGE_PATH}"/lcov.info" > ${__COVERAGE_PATH}"/prepared_report.info"
+sed -i "s/$OLD_PATH/$NEW_PATH/g" ${__COVERAGE_PATH}"/lcov.info"
 
-### Upload Coverage info to Codacy ###
-cat ${__COVERAGE_PATH}"/prepared_report.info" | codacy-coverage
-cat ${__COVERAGE_PATH}"/prepared_report.info" | coveralls
+## Upload Coverage info to Codacy ###
+cat ${__COVERAGE_PATH}"/lcov.info" | codacy-coverage
+cat ${__COVERAGE_PATH}"/lcov.info" | coveralls
 
+#OLD_PATH="Tests\/Backend\/Frontend"
+#NEW_PATH="Frontend"
+#sed "s/$OLD_PATH/$NEW_PATH/g" ${__COVERAGE_PATH}"/lcov.info" > ${__COVERAGE_PATH}"/prepared_report.info"
+#
+#### Upload Coverage info to Codacy ###
+#cat ${__COVERAGE_PATH}"/prepared_report.info" | codacy-coverage
+#cat ${__COVERAGE_PATH}"/prepared_report.info" | coveralls
 
 ### Cleanup! ###
 #remove all generated reports
