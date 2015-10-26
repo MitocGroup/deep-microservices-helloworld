abf816763dd8a297a3199225b5a581b11c8f09eb
vcernomschi <vcernomschi@161-242-209-188.globnet.md>
2015-10-26 06:36:36 +0200

    Add unit test for catchcatch() method

--- src/DeepHelloWorld/Tests/Frontend/angular/controllers/SayHelloController.spec.js
+++ src/DeepHelloWorld/Tests/Frontend/angular/controllers/SayHelloController.spec.js
@@ -5,6 +5,7 @@ import moduleName from '../../../../Frontend/js/app/angular/name';
 describe('Controllers', function() {
   let $controller;
   let $rootScope;
+  let controller = null;
 
   beforeEach(function() {
     angular.mock.module(moduleName);
@@ -18,9 +19,23 @@ describe('Controllers', function() {
   describe('SayHelloController', function() {
 
     it('Controller is implemented', function() {
-      let controller = $controller('SayHelloController', {$scope: {catchSubmit: null, name: 'test'}});
+      controller = $controller('SayHelloController', {$scope: {catchSubmit: null, name: 'test'}});
 
       expect(Object.keys(controller).length).toBe(2);
+    });
+
+    it('catchSubmit() method exists and executes without errors', function() {
+      let actualResult = null;
+      let error = null;
+
+      try {
+        actualResult = controller.catchSubmit();
+      } catch (e) {
+        error = e;
+      }
+
+      expect(error).toBe(null);
+      expect(actualResult).toBe(undefined);
     });
   });
 });
