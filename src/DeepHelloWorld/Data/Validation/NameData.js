f1d4987e5e0bd953d0690eb29d0537e4a66be492
AlexanderC <alexander.moldova@gmail.com>
2016-03-11 13:24:58 +0200

    Update NameData.js

--- src/DeepHelloWorld/Data/Validation/NameData.js
+++ src/DeepHelloWorld/Data/Validation/NameData.js
@@ -2,6 +2,6 @@
 
 module.exports = function(Joi) {
     return Joi.object().keys({
-        Name: Joi.string().alphanum().min(2).max(255).required()
+        Name: Joi.string().min(2).max(255).required()
     });
 };
