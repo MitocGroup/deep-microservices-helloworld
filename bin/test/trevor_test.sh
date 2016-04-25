5aec968b50ced5accb1f7f07c193c60db5b267c6
vcernomschi <vcernomschi@mitocgroup.com>
2016-04-25 15:16:07 +0300

    Update trevor_test

--- bin/test/trevor_test.sh
+++ bin/test/trevor_test.sh
@@ -5,12 +5,12 @@
 
 source $(dirname $0)/_head.sh
 
-npm link mocha
-npm link codacy-coverage
-npm link chai
-npm link jasmine-core
-npm link istanbul
-npm link istanbul-combine
+#npm link mocha
+#npm link codacy-coverage
+#npm link chai
+#npm link jasmine-core
+#npm link istanbul
+#npm link istanbul-combine
 npm link karma
 npm link karma-jspm
 npm link karma-jasmine
@@ -18,7 +18,7 @@ npm link karma-babel-preprocessor
 npm link karma-coverage
 npm link karma-verbose-reporter
 npm link karma-phantomjs-launcher
-npm link karma-ng-html2js-preprocessor
+#npm link karma-ng-html2js-preprocessor
 npm link isparta
 
 ######################
