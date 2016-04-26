ff9e64a77bb0a603564f193f8fa82c6f5405e5ae
vcernomschi <vcernomschi@mitocgroup.com>
2016-04-26 12:31:35 +0300

    Update corresponding to the new sceleton version

--- bin/test/preinstall.sh
+++ bin/test/preinstall.sh
@@ -3,8 +3,8 @@
 source $(dirname $0)/_head.sh
 
 npm install -g babel-cli@6.x.x &&\
-npm install -g babel-preset-es2015
-npm install -g babel-plugin-add-module-exports
+npm install -g babel-preset-es2015 &&\
+npm install -g babel-plugin-add-module-exports &&\
 npm install -g deepify &&\
 npm install -g jspm@0.16.15 &&\
 npm install -g browserify@11.2.x &&\
@@ -25,10 +25,11 @@ npm install -g karma-phantomjs-launcher@0.2.x &&\
 npm install -g karma-ng-html2js-preprocessor@0.2.x &&\
 npm install isparta@3.1.x
 
-if [ -v $TRAVIS_BUILD_ID ]; then
-    jspm config registries.github.auth $JSPM_GITHUB_AUTH_TOKEN
-else
+if [ -z $TRAVIS_BUILD_NUMBER ]; then
     echo "Running locally - no need to jspm config"
+else
+    echo "Running in CI - configuring jspm registries"
+    jspm config registries.github.auth $JSPM_GITHUB_AUTH_TOKEN
 fi
 
 bash `dirname $0`/phantomjs/install.sh
@@ -40,7 +41,8 @@ if [ "${__E2E_WITH_PUBLIC_REPO}" = "${E2E_TESTING}" ] || [ "${__E2E_WITH_PRIVATE
   npm install babel-cli@6.x.x &&\
   npm install babel-preset-es2015 &&\
   npm install babel-plugin-add-module-exports &&\
-  npm install jasmine2-custom-message@0.8.x
+  npm install jasmine2-custom-message@0.8.x &&\
+  npm install jasmine-utils@0.2.x
 fi
 
 if [ "${__E2E_WITH_PUBLIC_REPO}" = "${E2E_TESTING}" ]; then
