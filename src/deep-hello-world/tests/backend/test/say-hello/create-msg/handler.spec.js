491719553b70ff6d81562a947a36787430134aaa
vcernomschi <vcernomschi@mitocgroup.com>
2016-06-15 00:51:19 +0300

    Add backend unit tests for create-msg lambda

--- src/deep-hello-world/tests/backend/test/say-hello/create-msg/handler.spec.js
+++ src/deep-hello-world/tests/backend/test/say-hello/create-msg/handler.spec.js
@@ -1,13 +1,78 @@
-// THIS TEST WAS GENERATED AUTOMATICALLY ON Tue Jun 14 2016 18:31:05 GMT+0300 (EEST)
-
 'use strict';
 
 import chai from 'chai';
 import Handler from '../../../../../backend/src/say-hello/create-msg/Handler';
+import Kernel from
+  '../../../../../backend/src/say-hello/create-msg/node_modules/deep-framework/node_modules/deep-kernel';
+import Resource from
+  '../../../../../backend/src/say-hello/create-msg/node_modules/deep-framework/node_modules/deep-resource';
+import Log from
+  '../../../../../backend/src/say-hello/create-msg/node_modules/deep-framework/node_modules/deep-log';
+import KernelFactory from './../../common/KernelFactory';
 
-// @todo: Add more advanced tests
 suite('Handlers', () => {
+  let handler, kernelInstance;
+
   test('Class Handler exists in say-hello-create-msg module', () => {
     chai.expect(Handler).to.be.an('function');
   });
+
+  test('Load Kernel by using Kernel.load()', (done) => {
+    let callback = (backendKernel) => {
+      chai.assert.instanceOf(
+        backendKernel, Kernel, 'backendKernel is an instance of Kernel'
+      );
+
+      kernelInstance = backendKernel;
+
+      // complete the async
+      done();
+    };
+
+    KernelFactory.create({
+      Resource: Resource,
+      Log: Log,
+    }, callback);
+  });
+
+  test('Check Handler constructor', () => {
+    handler = new Handler(kernelInstance);
+
+    chai.expect(handler).to.be.an.instanceof(Handler);
+  });
+
+  test('Check handle throws "MissingRuntimeContextException"', () => {
+    let error = null;
+
+    try {
+      handler.handle({});
+    } catch (e) {
+      error = e;
+    }
+
+    chai.expect(error.constructor.name).to.equal('MissingRuntimeContextException');
+  });
+
+  test('Check handle return valid object', () => {
+    let request = {
+      Name: 'create-msg test',
+    };
+    handler._context = {
+      context: 'simpleContext',
+      invokedFunctionArn: 'test invokedFunctionArn',
+      failed: false,
+      succeeded: false,
+      succeed: function () {
+        this.succeeded = true;
+      },
+      fail: function () {
+        this.failed = true;
+      }
+    };
+
+    handler.handle(request);
+
+    chai.expect(handler.contextSent).to.equal(true);
+  });
+
 });
